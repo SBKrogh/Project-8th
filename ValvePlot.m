7db2b60654c9cb5b54098760a075c96ec8a1b79c
trojass <ignaciotrojaola@gmail.com>
2017-02-28 10:25:15 +0100

    Valve Plot

---
+++ ValvePlot.m
@@ -0,0 +1,26 @@
+fig=figure(1)
+c.ngl=3.2;
+c.OD=(0:0.01:1);
+a=c.OD.*90;
+a_min=0.31*90;
+a_max=90-a_min;
+Kv=zeros(1,length(a));
+
+for i=1:length(c.OD)
+
+if (a(i) <= a_min )
+   Kv(i)=0; 
+elseif ((a(i)-a_min)/(a_max) <= 1/c.ngl )
+Kv(i)=1*(a(i)-a_min)/(a_max)*c.ngl*exp(1-c.ngl);
+
+elseif ((a(i)-a_min)/(a_max) > 1/c.ngl)
+Kv(i)=1*exp(c.ngl*(((a(i)-a_min)/(a_max))-1));
+
+end    
+
+
+end
+plot(c.OD,Kv)
+title('Belimo R2015-1-S1')
+xlabel('OD [%]') % x-axis label
+ylabel('Kv(OD)/Kvs') % y-axis label
