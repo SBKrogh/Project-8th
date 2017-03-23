a716e10a0e42d5573ddb778bc505ab4d92a7c464
Nicolaj Vinkel Christensen <nvinkelc@gmail.com>
2017-03-23 14:59:03 +0100

    more senstools

--- Model/parameter estimation/senstools/measwater.m
+++ Model/parameter estimation/senstools/measwater.m
@@ -3,5 +3,5 @@ load('NameOfDataset.mat');
 t = 0:dt:((length(NameOfDataset)-1)*dt);
 t =t';
 y = NameOfDataset;
-u = (repmat(1,4,length(t)))';
+u = (repmat(1,8,length(t)))';
 save ('measwater.mat','t','y','u')
