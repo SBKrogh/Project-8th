406f27d98abdecea38347b3b8062d8bc691a9758
trojass <ignaciotrojaola@gmail.com>
2017-03-29 16:23:20 +0200

    Added output

--- Model/Symbolic model/f_vectorfunction.m
+++ Model/Symbolic model/f_vectorfunction.m
@@ -38,3 +38,25 @@ end
 warning('on','all')
 
 display('Solver finished')
+
+%% Obtain de pressure difference
+
+for i = 1:N
+   f(:,i) = -pipe(Cp,i,B_1,U,z) - elev(delta_z,i,U) - valve(Cv,i,B_1,U,z) + dPpump(U,i);     
+end
+
+DeltaP = f;
+
+%% Set the output 
+
+y = zeros(lenght(U),8);
+
+y(:,1) = DeltaP(:,8);     % Edge 1 Component C2
+y(:,2) = DeltaP(:,11);    % Edge 8 Component 16
+y(:,3) = DeltaP(:,12) + DeltaP(:,13) + DeltaP(:,16) + DeltaP(:,17);    % Edge 9 Component 18 & Edge 10 Component 19 & Edge 14 Component 23 & Edge 15 Component 24
+y(:,4) = DeltaP(:,18) + DeltaP(:,19) + DeltaP(:,6) + DeltaP(:,23);    % Edge 16 Component 25 & Edge 17 Component 26 & Edge 21 Component 30 & Edge 22 Component 31
+y(:,5) = DeltaP(:,17);    % Edge 15 Component 24
+y(:,6) = DeltaP(:,15) + DeltaP(:,14);    % Edge 13 Component 20 & Edge 12 Component 21
+y(:,7) = DeltaP(:,23);    % Edge 22 Component 31
+y(:,8) = DeltaP(:,21) + DeltaP(:,22);    % Edge 19 Component 28 & Edge 20 Component 27
+
