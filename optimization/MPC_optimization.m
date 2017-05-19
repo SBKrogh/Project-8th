e0a23f376b8d5aea4198907e4a38bdcaeec66144
Krisztian Balla <cballa7@gmail.com>
2017-05-19 22:37:03 +0200

    Working optimization with all constraints

--- optimization/MPC_optimization.m
+++ optimization/MPC_optimization.m
@@ -6,19 +6,17 @@ clc
 
 %Variabels that we need to sort out.
 % delta_p_0 d_hp U_bar_hp q_bar_p_hp
-% 
 
-% Variabels that we hare in the Matrix_op.m file
+% Variabels that we use in the Matrix_op.m file
 % Lambda_A Lambda_B Lambda_C Gamma Psi Phi Theta Omega Pi 
 Matrix_op;
 trock;                  %Load system matrices
 
-delta_p_0 = 0.02;      %small-signal initial WT pressure measurement
+delta_p_0 = 0.05;      %small-signal initial WT pressure measurement
 
 d_hp = 0.05*90*ones(48,1);      % Small signal deviation of the Valves OD
 U_bar_hp = 0.2*ones(48,1);      % Input pressure to ring pumps operating point
 q_bar_p_hp = 1.4*ones(48,1);    % Flow operating point of the ring pumps
-
 
 %%%%%%%%%%%%%%%%%% Generel QP %%%%%%%%%%%%%%%%%%
 % 0.5*x'R*x+f'*x
@@ -35,7 +33,6 @@ y_bar = [0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562;
         0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;
         0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;
         0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991 ;0.1562; 0.0991];    
-%Todo-Fix: between y1 and y2 -> huge difference
 
 y1 = y_low - y_bar - Theta*Phi*delta_p_0-(Theta*Psi+Pi)*d_hp;
 y2 = y_high - y_bar - Theta*Phi*delta_p_0-(Theta*Psi+Pi)*d_hp;
@@ -63,14 +60,16 @@ R2=(R-R')/2;
 f = U_bar_hp'*(Lambda_A+Lambda_C*Gamma)+d_hp'*(Lambda_B+Lambda_C*Psi)'+delta_p_0*(Lambda_C*Phi)'+q_bar_p_hp';
 
 %CONSTRAINTS FOR Y AND X
-% A = [-L_y; L_y; -Gamma;Gamma];
-% b = [y1; y2; delta_p_wt_1; delta_p_wt_2];
-% 
+A = [-L_y; L_y; -Gamma;Gamma];
+b = [y1; y2; delta_p_wt_1; delta_p_wt_2];
+
+%CONSTRAINTS FOR Y
 % A = [-L_y; L_y];
 % b = [y1; y2];
 
-A = [-Gamma;Gamma];
-b = [delta_p_wt_1; delta_p_wt_2];
+%CONSTRAINTS FOR X
+% A = [-Gamma;Gamma];
+% b = [delta_p_wt_1; delta_p_wt_2];
 
 lb = u_low*ones(48,1);
 ub = u_high*ones(48,1);
