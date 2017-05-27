fb272095163001dc7bd3534c341dbb6089d33617
trojass <ignaciotrojaola@gmail.com>
2017-05-27 20:23:18 +0200

    New model implementation

--- optimization/Simulation_kind of working/mat_est.m
+++ optimization/Simulation_kind of working/mat_est.m
@@ -1,13 +1,23 @@
-function [ M , N, As, Bs, Cs, Ds, S, C1, C2] = mat_est
+function [ M , N, A_c, B_c, C_c, D_c ] = mat_est
 
 % Load the parameters saved from the estimation
-linear_b_est = load('est_newtrock.mat');
-linear_b_est = linear_b_est.linear_b_est;
+load('M_n.mat')
+load('C_D')
 matrices;
+pipe_param;
 
 A = diag([linear_b_est(1) linear_b_est(2) linear_b_est(3) linear_b_est(4) linear_b_est(5) linear_b_est(6) linear_b_est(7)...
     linear_b_est(8) 0 linear_b_est(9) 0 0 linear_b_est(10) linear_b_est(11) linear_b_est(12) linear_b_est(13)...
     linear_b_est(14) 0 linear_b_est(15) linear_b_est(16) linear_b_est(17) linear_b_est(18) linear_b_est(19) linear_b_est(20)]);
+
+N = [ linear_b_est(21)   linear_b_est(22)   linear_b_est(23)   linear_b_est(24)   1   linear_b_est(25)   linear_b_est(26)   linear_b_est(27) ;
+    linear_b_est(28)   linear_b_est(29)    linear_b_est(30)   linear_b_est(31)   linear_b_est(32)   linear_b_est(33)   1   linear_b_est(34);
+    linear_b_est(35)   linear_b_est(36)    linear_b_est(37)    linear_b_est(38)   linear_b_est(39)   linear_b_est(40) -1 1;
+    linear_b_est(41)   linear_b_est(42) linear_b_est(43)  linear_b_est(44)   linear_b_est(45)   1   linear_b_est(46)   1;
+    linear_b_est(47)   linear_b_est(48)   linear_b_est(49)   linear_b_est(50)   linear_b_est(51)   1    linear_b_est(52)  linear_b_est(53) ;
+    linear_b_est(54)    linear_b_est(55)    linear_b_est(56)   linear_b_est(57)   linear_b_est(58)   linear_b_est(59)   linear_b_est(60)   linear_b_est(61);
+    linear_b_est(62)   linear_b_est(63)   linear_b_est(64) linear_b_est(65)   linear_b_est(66)   linear_b_est(67)   linear_b_est(68)   linear_b_est(69);
+    linear_b_est(70) linear_b_est(71)  linear_b_est(72)    linear_b_est(73)   linear_b_est(74)   linear_b_est(75)   linear_b_est(76)   linear_b_est(77)];
 
 
 M = B_1 * A * B_1';
@@ -29,40 +39,48 @@ M = B_1 * A * B_1';
 %      
 %      0      linear_b_est(8)      linear_b_est(13) linear_b_est(17) 0 linear_b_est(20) linear_b_est(22) linear_b_est(23)];
  
-    
-N = [ linear_b_est(95)   linear_b_est(96)   linear_b_est(97)   linear_b_est(98)   1   linear_b_est(99)   linear_b_est(100)   linear_b_est(101) ;
-    linear_b_est(102)   linear_b_est(21)    linear_b_est(103)   linear_b_est(104)   linear_b_est(105)   linear_b_est(106)   1   linear_b_est(107);
-    linear_b_est(108)   linear_b_est(22)    linear_b_est(23)    linear_b_est(109)   linear_b_est(110)   linear_b_est(111) -1 1;
-    linear_b_est(112)   linear_b_est(113) linear_b_est(24)  linear_b_est(114)   linear_b_est(115)   1   linear_b_est(116)   1;
-    linear_b_est(117)   linear_b_est(118)   linear_b_est(119)   linear_b_est(120)   linear_b_est(121)   1    linear_b_est(122)  linear_b_est(123) ;
-    linear_b_est(25)    linear_b_est(26)    linear_b_est(124)   linear_b_est(125)   linear_b_est(126)   linear_b_est(127)   linear_b_est(128)   linear_b_est(129);
-    linear_b_est(130)   linear_b_est(131)   linear_b_est(27) linear_b_est(28)   linear_b_est(132)   linear_b_est(133)   linear_b_est(134)   linear_b_est(135);
-    linear_b_est(136) linear_b_est(29)  linear_b_est(30)    linear_b_est(137)   linear_b_est(138)   linear_b_est(139)   linear_b_est(140)   linear_b_est(141)];
-    
-C1 = [ linear_b_est(31)  linear_b_est(32) linear_b_est(33) linear_b_est(34) linear_b_est(35) linear_b_est(36) linear_b_est(37) linear_b_est(38);
-    linear_b_est(39)  linear_b_est(40) linear_b_est(41) linear_b_est(42) linear_b_est(43) linear_b_est(44) linear_b_est(45) linear_b_est(46);
-    linear_b_est(47)  linear_b_est(48) linear_b_est(49) linear_b_est(50) linear_b_est(51) linear_b_est(52) linear_b_est(53) linear_b_est(54);
-    linear_b_est(55)  linear_b_est(56) linear_b_est(57) linear_b_est(58) linear_b_est(59) linear_b_est(60) linear_b_est(61) linear_b_est(62)];
+C1 = [ est_C_D(1)  est_C_D(2) est_C_D(3) est_C_D(4) est_C_D(5) est_C_D(6) est_C_D(7) est_C_D(8);
+    est_C_D(9)  est_C_D(10) est_C_D(11) est_C_D(12) est_C_D(13) est_C_D(14) est_C_D(15) est_C_D(16);
+    est_C_D(17)  est_C_D(18) est_C_D(19) est_C_D(20) est_C_D(21) est_C_D(22) est_C_D(23) est_C_D(24);
+    est_C_D(25)  est_C_D(26) est_C_D(27) est_C_D(28) est_C_D(29) est_C_D(30) est_C_D(31) est_C_D(32)];
 
-C2 = [ linear_b_est(63)  linear_b_est(64) linear_b_est(65) linear_b_est(66) linear_b_est(67) linear_b_est(68) linear_b_est(69) linear_b_est(70);
-    linear_b_est(71)  linear_b_est(72) linear_b_est(73) linear_b_est(74) linear_b_est(75) linear_b_est(76) linear_b_est(77) linear_b_est(78);
-    linear_b_est(79)  linear_b_est(80) linear_b_est(81) linear_b_est(82) linear_b_est(83) linear_b_est(84) linear_b_est(85) linear_b_est(86);
-    linear_b_est(87)  linear_b_est(88) linear_b_est(89) linear_b_est(90) linear_b_est(91) linear_b_est(92) linear_b_est(93) linear_b_est(94)];
+C2 = [ est_C_D(33)  est_C_D(34) est_C_D(35) est_C_D(36) est_C_D(37) est_C_D(38) est_C_D(39) est_C_D(40);
+    est_C_D(41)  est_C_D(42) est_C_D(43) est_C_D(44) est_C_D(45) est_C_D(46) est_C_D(47) est_C_D(48);
+    est_C_D(49)  est_C_D(50) est_C_D(51) est_C_D(52) est_C_D(53) est_C_D(54) est_C_D(55) est_C_D(56);
+    est_C_D(57)  est_C_D(58) est_C_D(59) est_C_D(60) est_C_D(61) est_C_D(62) est_C_D(63) est_C_D(64)];
 
-WTconstant = (pi*0.32^2 * 10^5)/(1000*9.8);
-
-
-B2 =  B_0;
-
+WTconstant = (pipe.e26.area * 10^5)/(1000*9.8);
+% 
+% 
 S =  (1/WTconstant) * pinv(H_0) * H_1 * B_1';
 
-As =  S * (M \ B2);
+N_c = [ N(:,5) N(:,6)];
 
-Bs =  -S * (M \ N);
+Q_c = [ N(:,2) N(:,4)];
 
-Cs =  -[ C1 * ( M \ B2 ); 1];
+C11 = [ C1(2,:); C1(4,:)];
 
-Ds =  [C2 - C1 * ( M \ N ); zeros(1,8)];
+C22 = [C2(2,2) C2(2,4);
+    C2(4,2) C2(4,4)];
+
+
+A_c =  S * (M \ B_0);
+
+E_s = S * (M \ Q_c);
+
+B_s = - S * (M \ N_c);
+
+D_s = [C11 * (M\N_c) ];
+D_s(1,:) = -D_s(1,:);
+D_s(2,1) = - D_s(2,1);
+
+
+C_c =  [- C11 * ( M \ B_0 )];
+
+K_s =  [C22 - C11 * ( M \ Q_c ); zeros(1,2)];
+
+B_c = [B_s E_s];
+D_c = [D_s K_s];
 
 
 end
--- optimization/trock.m
+++ optimization/trock.m
@@ -1,39 +1,39 @@
 
 % Complete the state-space system matrices for estimation
-[ M , N, As, Bs, Cs, Ds, S, C1, C2] = mat_est;
+[ M_c , N_c, Q_c, Ac, Bcc, Cc, Dcc,] = mat_est;
 
-M_c = M;
-
-N_c = [ N(:,5) N(:,6) ];
-
-Q_c =[ N(:,2) N(:,4)];
-
-C11 = [ C1(2,:); C1(4,:)];
+% M_c = M;
+% 
+% N_c = [ N(:,5) N(:,6) ];
+% 
+% Q_c =[ N(:,2) N(:,4)];
+% 
+% C11 = [ C1(2,:); C1(4,:)];
 
 % C22 = [C2(2,2); C2(2,4)];
 
-Ac = S* (M_c \ B_0);
-Bc = -S* (M_c \ N_c);
-Ec = S* (M_c \ Q_c);
-Cc = -C11* (M_c \ B_0);
-Dc = C11* (M_c \ N_c);
+% Ac = S* (M_c \ B_0);
+% Bc = -S* (M_c \ N_c);
+% Ec = S* (M_c \ Q_c);
+% Cc = -C11* (M_c \ B_0);
+% Dc = C11* (M_c \ N_c);
 % Kc = C22 - C11* (M_c \ Q_c)
-Ds = [Ds(2,2) Ds(2,4) Ds(2,5) Ds(2,6);
-    Ds(4,2) Ds(4,4) Ds(4,5) Ds(4,6)];
-Ts = 6.4385;  %Note: Rethink this %
+% Ds = [Ds(2,2) Ds(2,4) Ds(2,5) Ds(2,6);
+%     Ds(4,2) Ds(4,4) Ds(4,5) Ds(4,6)];
+Ts = 87.5440;  %Note: Rethink this %
 
-Bcc = [Bc Ec];
+% Bcc = [Bc Ec];
 % Dcc = [Dc Kc];
 
-sysc = ss(Ac,Bcc,Cc,Ds);
+sysc = ss(Ac,Bcc,Cc,Dcc);
 sysdd = c2d(sysc, Ts,'zoh');
 
 Ad = sysdd.A;
 Bd = sysdd.B(1:2);
 Ed = sysdd.B(3:4);
 Cd = sysdd.C;
- Dd = sysdd.D(:,3:4);
- Kd = sysdd.D(:,1:2);
+Dd = sysdd.D(:,1:2);
+Kd = sysdd.D(:,3:4);
 % 
 % sys = ss(As,Bs,Cs,Ds);
 % sysd = c2d(sys, Ts,'zoh');
@@ -62,8 +62,8 @@ Cd = sysdd.C;
 
 %Kd = [ sysdd.D(1,1) sysdd.D(1,2); sysdd.D(2,1) sysdd.D(2,2) ];
 
-E_new = Ed*pinv(Kd);     
+% E_new = Ed*pinv(Kd);     
 
 %WTconstant = (pi * (0.32)^2)/(1000*9.8);
 
-save('sysd.mat','Ad','Bd','E_new','Cd','Dd','Ts')
+% save('sysd.mat','Ad','Bd','E_new','Cd','Dd','Ts')
